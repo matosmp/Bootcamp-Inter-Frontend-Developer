9d7c72a5004489c278b3585017599fa774cc0009
Fernando Matos <matos.fernandomp@gmail.com>
2022-02-12 11:53:38 -0300

    Tipos boolean,unio e any

---
+++ src/type_any.ts
@@ -0,0 +1,22 @@
+/** Seção 3 : Trabalhando com tipos
+ * 
+ * Aula 14 - type ANY
+ * 
+ * O type ANY aceita qualquer tipo, ou seja, não faz é interessante utilizar com typeScript, EXCETO em situações muito
+ * específicas. 
+ * 
+ */
+
+let precoProdutos;
+
+precoProdutos = true;
+precoProdutos = "R$";
+precoProdutos = 100;
+
+let nota1:any;
+let nota2:any;
+
+nota1 = 10;
+nota2 = 20;
+
+nota1="teste"
