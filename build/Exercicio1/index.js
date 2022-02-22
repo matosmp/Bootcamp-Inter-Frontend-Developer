ee338aea5ad9369a25c4bbe4776adc3af13c18de
Fernando Matos <matos.fernandomp@gmail.com>
2022-02-21 22:00:11 -0300

    Exercício

---
+++ build/Exercicio1/index.js
@@ -0,0 +1,21 @@
+"use strict";
+const input1 = document.getElementById('num1');
+const input2 = document.getElementById('num2');
+const buttonSomar = document.getElementById('buttonSomar');
+const buttonSubtrair = document.getElementById('buttonSubtrair');
+function somar(a, b) {
+    return a + b;
+}
+;
+buttonSomar.addEventListener('click', function () {
+    const resultado = somar(Number(input1.value), Number(input2.value));
+    console.log(resultado);
+});
+function subtrair(a, b) {
+    return a - b;
+}
+;
+buttonSubtrair.addEventListener('click', function () {
+    const resultado = subtrair(Number(input1.value), Number(input2.value));
+    console.log(resultado);
+});
